b1474321b07f38d29f92e1287188f2502c21b5d4
Crypto_Vdim <vadimrace@gmail.com>
2023-01-02 22:09:01 -0500

    started js functions

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
 //pseudocode
 //review mod 4
-//1. pull in timer function from lesson x
-//2. pull in generate button function from last assignment
-//3. pull in the object lesson code x
-//4. make the questions in the object lesson code x
-//5. style timer to the right side, change start time, store final score
+//1. hide splashPage details when executeQUiz() is called
+//2. 
+//3. show first question out of the quiz questions on the screen
+//4. 
+//5. 
 //6. create additional index.html for high scores
 //7. 
-
+var startQuizBtn = document.querySelector('#startQuiz');
+startQuizBtn.addEventListener('click', executeQuiz);
 var quizQuestions = [
   question1 = {
     question: 'Blah Blah Blah',
@@ -31,6 +32,18 @@ var quizQuestions = [
     D: 'stix answer'
   },
 ];
+
+function hideSplashPage(){
+  const splashPageRemoval = document.getElementsByClassName("#splashPage");
+  console.log(splashPageRemoval);
+  splashPageRemoval.remove();
+}
+
+function executeQuiz(){
+  console.log("You started the quiz");
+  setTime();
+  hideSplashPage();
+}
 
 // Selects element by class
 var timeEl = document.querySelector(".time");
@@ -61,41 +74,12 @@ function sendMessage() {
   timeEl.textContent = " ";
   var imgEl = document.createElement("img");
   imgEl.setAttribute("src", "./assets/images/image_1.jpg");
-  mainEl.appendChild(imgEl);
+  // mainEl.appendChild(imgEl);
 
 }
 
-setTime();
 
 // setInterval(function(){
 //   console.log("Hello")
 // }, 5000)
-
-var planet = {
-    name: "Earth",
-    age: "4.543 billion years",
-    moons: 1,
-    isPopulated: true,
-    population: "7.594 billion",
-    // Objects can store arrays in a key-value pair
-    neighboringPlanets: ["Mars", "Venus"],
-    // Objects can also store methods
-    tellFunFact: function () {
-      console.log("The earth is the only planet in our solar system not named after a Roman god or goddess.");
-    },
-    showWarning: function () {
-      console.log("Space junk falls into Earth's atmosphere at a rate of about one a day.");
-    }
-  };
-  
-  // To access a value in an array, use the name of the object, the key and the index.
-  // Logs "Mars" using dot notation
-  console.log(planet.neighboringPlanets[0]);
-  
-  // Logs "Mars" using bracket notation
-  console.log(planet["neighboringPlanets"][0]);
-  
-  // To call a method, use the name of the object and the key. Don't forget the ()!
-  planet.tellFunFact();
-  planet.showWarning();
     
