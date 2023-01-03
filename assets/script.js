a57d2afcbb8a343532389639d99adf5e609f9335
Crypto_Vdim <vadimrace@gmail.com>
2023-01-02 20:59:24 -0500

    Created quiz splash page with time

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,42 @@
 //pseudocode
 //review mod 4
-//1. pull in timer function from lesson
+//1. pull in timer function from lesson x
 //2. pull in generate button function from last assignment
-//3. pull in the object lesson code
-//4. make the questions in the object lesson code
+//3. pull in the object lesson code x
+//4. make the questions in the object lesson code x
 //5. style timer to the right side, change start time, store final score
-//6. wait for end of mod 4 to store data server side  
+//6. create additional index.html for high scores
+//7. 
 
+var quizQuestions = [
+  question1 = {
+    question: 'Blah Blah Blah',
+    A: 'one answer',
+    B: 'two answer',
+    C: 'three answer',
+    D: 'four answer'
+  },
+  question2 = {
+    question: "Me Me Me",
+    A: 'this answer',
+    B: 'that answer',
+    C: 'C answer',
+    D: 'D answer'
+  },
+  question3 = {
+    question: "La La La",
+    A: 'in answer',
+    B: 'out answer',
+    C: 'fix answer',
+    D: 'stix answer'
+  },
+];
 
 // Selects element by class
 var timeEl = document.querySelector(".time");
 
 // Selects element by id
-var mainEl = document.getElementById("main");
+var mainEl = document.getElementById("colorSplosion");
 
 var secondsLeft = 10;
 
@@ -20,11 +44,11 @@ function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+    timeEl.textContent = "Time: "+secondsLeft;
 
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
-      clearInterval(timerInterval);
+      // clearInterval(timerInterval);
       // Calls function to create and append image
       sendMessage();
     }
