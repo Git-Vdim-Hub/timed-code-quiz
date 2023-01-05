f838783a6417c3e0197b37f40ec0430ba4892fd2
Crypto_Vdim <vadimrace@gmail.com>
2023-01-05 17:28:14 -0500

    worked on endQuiz function, fixed flow logic

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 //pseudocode
 //review mod 4
-//1. fix logic error between functions to check answer
 //2. create the High Score entry field
 //3. create the scoresheet
 //4. add two buttons for the score sheet to either clear it or go back
 //  a store the scores in local storage
 //5. Create questions
+//6. properly link the "View HighScores"
+//7. annotate if answer is right or wrong
+//8. write comments
+//9. write readme
 
 
 
@@ -18,6 +21,8 @@ var selectedAnswer;
 var questionNumber = 0;
 var secondsLeft = 25;
 var score=0;
+var quizComplete=false;
+var timerInterval;
 
 var quizQuestions = [
   question1 = {
@@ -58,7 +63,7 @@ function displaySplashPage(){
         </p>
     </div>
     <div class="startButtonContainer">
-    <button id="startQuiz" class="startButton">
+    <button id="startQuiz" class="button">
       Start Quiz
     </button>
   </div>
@@ -67,11 +72,9 @@ function displaySplashPage(){
 
 
 function executeQuiz(){
-  console.log("You started the quiz");
   setTime();
   showQuestion(questionNumber);
   checkQuestion();
-  // questionNumber++;
 }
 
 function showQuestion(question){
@@ -79,18 +82,13 @@ function showQuestion(question){
     <div class="questionContainer">
       <div class="questions">
       <h1 class="question">${quizQuestions[question].question}</h1>
-       <div id="A">1. ${quizQuestions[question].A}</div>
-       <div id="B">2. ${quizQuestions[question].B}</div>
-       <div id="C">3. ${quizQuestions[question].C}</div>
-       <div id="D">4. ${quizQuestions[question].D}</div>
+       <div id="ans">1. ${quizQuestions[question].A}</div>
+       <div id="ans">2. ${quizQuestions[question].B}</div>
+       <div id="ans">3. ${quizQuestions[question].C}</div>
+       <div id="ans">4. ${quizQuestions[question].D}</div>
       </div>
     </div>  
   `;
-}
-
-function endQuiz(){
-  splash.innherHTML='';
-  console.log(splash.innherHTM);
 }
 
 function checkQuestion(){
@@ -98,42 +96,70 @@ function checkQuestion(){
     selectedAnswer = event.target.innerHTML;
     console.log(selectedAnswer);
     console.log(quizQuestions[questionNumber].Ans)
-    if(selectedAnswer!==quizQuestions[questionNumber].Ans){
+    if(selectedAnswer==quizQuestions[questionNumber].Ans){
       score+=5;
     }else{
       secondsLeft-=5;
     }
     questionNumber++;
     showQuestion(questionNumber);
-    if(questionNumber<=(quizQuestions.length-1)){
+    if(questionNumber<(quizQuestions.length-1)){
       console.log(questionNumber);
       console.log(quizQuestions.length-1);
       checkQuestion();
     } else{
-      score+=secondsLeft;
-      endQuiz();
+      document.querySelector('.questions').addEventListener('click', function(event){
+        selectedAnswer = event.target.innerHTML;
+        console.log(selectedAnswer);
+        console.log(quizQuestions[questionNumber].Ans)
+        if(selectedAnswer==quizQuestions[questionNumber].Ans){
+          score+=5;
+        }else{
+          secondsLeft-=5;
+        }
+        quizComplete=true;
+        score+=secondsLeft;
+      });
     }
     
   })
 };
 
+function endQuiz(){
+  // Stops execution of action at set interval
+  clearInterval(timerInterval);
+  splash.innerHTML=`
+  <div class="questionContainer">
+    <div class="questions">
+      <h1 class="question">All done!</h1>
+      <div> Your final score is ${score}</div>
+    </div>
+    <div>Enter Initials: </div>
+    <input type="text" id="initials">
+      <button id="submitBtn" class="button">
+      Submit
+    </button>
+  </div>  
+`;
+const submitBtn = document.querySelector('#submitBtn');
+var initialsInput = document.querySelector("initials");
+submitBtn.addEventListener("click", function(event){
+  console.log("You clicked the submit button");
+  var studentInitials = initialsInput.innerHTML;
+  console.log("studentInitials");
+});
+}
+
 function setTime() {
   // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
+  timerInterval = setInterval(function() {
     timeEl.textContent = "Time: "+secondsLeft;
+    secondsLeft--;
 
-    if(secondsLeft <= 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
+    if(secondsLeft <= 0 || quizComplete) {
+      secondsLeft = 0;
+      timeEl.textContent = "Time: "+secondsLeft;
       endQuiz();
     }
-
   }, 1000);
-}
-
-function endQuiz(){
-
-}
-    
+}  
